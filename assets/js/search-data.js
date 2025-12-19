c0da18667e82a267d73ea4bdad0feef88ce21feb
justinaw04 <115896840+justinaw04@users.noreply.github.com>
2025-12-19 21:21:29 +0000

    Deploying to gh-pages from @ justinaw04/justinaw04.github.io@8ae2c1be76e8c94b99493a28d3b85268a8a3f4f8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-bookshelf",
-          title: "bookshelf",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/books/";
-          },
-        },{id: "post-a-post-with-plotly-js",
+  },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
         
