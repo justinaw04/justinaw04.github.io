9008827fa7980b8aa0854b4e971f581d4da9a13d
justinaw04 <115896840+justinaw04@users.noreply.github.com>
2025-12-19 20:48:55 +0000

    Deploying to gh-pages from @ justinaw04/justinaw04.github.io@80766fabff77cd901e974ef08e457df3a1a40799 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,7 +9,14 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-submenus",
+  },{id: "nav-bookshelf",
+          title: "bookshelf",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/books/";
+          },
+        },{id: "nav-submenus",
           title: "submenus",
           description: "",
           section: "Navigation",
