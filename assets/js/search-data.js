2662b2e84310d8520980d57384cc376cd3301004
justinaw04 <115896840+justinaw04@users.noreply.github.com>
2025-12-19 20:31:52 +0000

    Deploying to gh-pages from @ justinaw04/justinaw04.github.io@9790fcbb9bf5246f59bb40eaa3b69550b56b5ad5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,11 +440,6 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
             },},{id: "news-graduated-from-uw-madison-badger",
           title: 'Graduated from UW-Madison :badger:',
           description: "",
